78126581339d1b14739f8ee1100e0bc3954cdc8c
ZionAmar <zion0549774827@hotmail.com>
2024-08-04 01:37:09 +0300

    קובץ ראשי מתוקן

---
+++ main_switch.ino
@@ -0,0 +1,19 @@
+#define Switch_pin D2
+
+bool IamServer;
+
+void setup() {
+  pinMode(Switch_pin, INPUT_PULLUP);
+  IamServer = (digitalRead(Switch_pin) == LOW) ? true : false;
+  if (IamServer)
+    performance_setup();
+  else
+    game_setup();
+}
+
+void loop(){
+    if (IamServer)
+    performance_loop();
+  else
+    game_loop();
+}
