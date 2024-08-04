785a107d03e88936b14b9dfc7e3237d61f0c7f00
ZionAmar <zion0549774827@hotmail.com>
2024-08-04 22:48:15 +0300

    עודכן קוד ללא שרת הבודק לחיצה הכי קצרה ונותן חיוי בהתאם

--- Arduino-game.ino
+++ Arduino-game.ino
@@ -6,15 +6,18 @@ void setup() {
   pinMode(Switch_pin, INPUT_PULLUP);
   IamServer = (digitalRead(Switch_pin) == LOW) ? true : false;
   if (IamServer)
-    performance_setup();
-  else
-    game_setup();
+    // performance_setup();
+        game_setup();
+
+  // else
+  //   game_setup();
 }
 
 void loop(){
     if (IamServer)
-    performance_loop();
-  else
-    game_loop();
-}
+    // performance_loop();
+        game_loop();
 
+  // else
+  //   game_loop();
+}
--- game_mode.ino
+++ game_mode.ino
@@ -1,27 +1,58 @@
-#define pinLed_R D1
+// #define pinLed_R D1
 #define pinLed_G D3
 #define pinLed_B D4
+#define pinBtn D7
+
+int lastVal;
+unsigned long lastPress;
+unsigned long duration;
+unsigned long lastDuration;
 
 void game_setup() {
+  Serial.begin(9600);
   pinMode(pinLed_R, OUTPUT);
   pinMode(pinLed_G, OUTPUT);
   pinMode(pinLed_B, OUTPUT);
+  pinMode(pinBtn, INPUT_PULLUP);
+  LedOFF();
+  lastVal = HIGH;
+  lastPress = millis();
 }
 
 void game_loop() {
-  LightLed(0x5DE2E7);
-  delay(3000);
-  LightLed(0xFE9900);
-  delay(3000);
+  if (digitalRead(pinBtn) == LOW && lastVal == HIGH && (millis() - lastPress > 50)) {
+    lastPress = millis();
+    lastVal = LOW;
+    Serial.println("start press");
+  }
+  if (digitalRead(pinBtn) == HIGH && lastVal == LOW) {
+    lastVal = HIGH;
+    duration = millis() - lastPress;
+    if(!lastDuration) lastDuration = duration;
+    Serial.println("end press");
+    Serial.print("Duration: ");
+    Serial.println(duration);
+    if (duration < lastDuration) {
+      lastDuration = duration;
+      LightLed(0x5DE2E7); 
+    } else {
+      LightLed(0xFE9900);
+    }
+  }
 }
 
 void LightLed(long HexaColor) {
-  // HexaColor - RRGGBB
   int red = (HexaColor >> 16) & 0xFF;
   int green = (HexaColor >> 8) & 0xFF;
   int blue = HexaColor & 0xFF;
-  
+
   analogWrite(pinLed_R, red);
   analogWrite(pinLed_G, green);
   analogWrite(pinLed_B, blue);
+}
+
+void LedOFF() {
+  analogWrite(pinLed_R, 0);
+  analogWrite(pinLed_G, 0);
+  analogWrite(pinLed_B, 0);
 }
